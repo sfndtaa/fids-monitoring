899e6fc125ed93ccf57b55b2776f163a04a3d473
sfndtaa <sofi.nandita02@gmail.com>
2026-09-01 16:08:38 +0800

    redesign

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var list<string> 
      */
     protected $hidden = [
         'password',
